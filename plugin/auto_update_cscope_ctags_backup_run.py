96b388f6bc32c6a5d601bc298c14f6cb58f59fb7
haolong.zhang <haolong.zhang@ck-telecom.com>
2017-05-03 16:22:13 +0800

    1:multithreading for gen cscope and ctag database 2:consistency for cscope parm between normal project and kernel project

--- plugin/auto_update_cscope_ctags_backup_run.py
+++ plugin/auto_update_cscope_ctags_backup_run.py
@@ -4,6 +4,7 @@ import glob
 import sys
 import time
 import getpass
+import threading
 
 arch_parameter_list = ['normal', 'alpha', 'arm', 'avr32', \
         'c6x', 'frv', 'hexagon', 'm68k', 'microblaze', 'mn10300', \
@@ -164,6 +165,34 @@ def gen_cscope_and_ctag_file():
         debug_backrun_python_print("show print_message")
         print_cscope_and_ctags_info = 1
 
+    # add thread
+    cscope_task = threading.Thread(target = cscope_task_func, args = (print_cscope_and_ctags_info, start_time))
+    ctags_task = threading.Thread(target = ctags_task_func, args = (print_cscope_and_ctags_info, start_time))
+
+    cscope_task.start()
+    ctags_task.start()
+
+    cscope_task.join()
+    ctags_task.join()
+    #end thread
+
+    clear_lock_i()
+    all_take_time  = time.time() - start_time
+    if 1 == print_cscope_and_ctags_info:
+        Warnin_print("All finish take %s s" % all_take_time)
+    else:
+        debug_backrun_python_print("All finish take %s s" % all_take_time)
+
+    gnome_osd_print('%s project update tags end' % sys.argv[1])
+
+def clear_lock_i():
+    end_remove_lock_cmd = "rm .auto_cscope_ctags/lock 1>/dev/null  2>&1"
+    debug_backrun_python_print("now for remove_lock_cmd")
+    os.system(end_remove_lock_cmd)
+    debug_backrun_python_print("end for remove_lock_cmd")
+
+def cscope_task_func(show_message_enable, s_time):
+
     if 'normal' == sys.argv[1]:
         normal_cmd = "find . -name '*.c' "
         for i_care_type in care_file_type:
@@ -171,19 +200,18 @@ def gen_cscope_and_ctag_file():
 
         normal_cmd = normal_cmd + " -o -type f -name '*config'"
         normal_cmd = normal_cmd + "> cscope.files "
-        normal_cmd = normal_cmd + ";cscope -bkq -i cscope.files "
-        if 0 == print_cscope_and_ctags_info:
+        normal_cmd = normal_cmd + ";cscope -bkq -i cscope.files -f cscope.out"
+        if 0 == show_message_enable:
             normal_cmd = normal_cmd + " 1>/dev/null  2>&1"
         else:
             Warnin_print(normal_cmd)
-
 
         debug_backrun_python_print(normal_cmd)
         debug_backrun_python_print("now for cscope")
         os.system(normal_cmd)
         normal_end_time  = time.time()
-        use_time_str = "cscope Use time = %s s" % (normal_end_time - start_time)
-        if 1 == print_cscope_and_ctags_info:
+        use_time_str = "cscope Use time = %s s" % (normal_end_time - s_time)
+        if 1 == show_message_enable:
             Warnin_print(use_time_str)
         else:
             debug_backrun_python_print(use_time_str)
@@ -191,7 +219,7 @@ def gen_cscope_and_ctag_file():
         debug_backrun_python_print("end for cscope")
     else:
         kernel_cmd = "make cscope ARCH=%s" % sys.argv[1]
-        if 0 == print_cscope_and_ctags_info:
+        if 0 == show_message_enable:
             kernel_cmd = kernel_cmd + " 1>/dev/null  2>&1"
         else:
             Warnin_print(kernel_cmd)
@@ -200,12 +228,45 @@ def gen_cscope_and_ctag_file():
         debug_backrun_python_print("now for cscope")
         os.system(kernel_cmd)
         normal_end_time  = time.time()
-        use_time_str = "cscope Use time = %s s" % (normal_end_time - start_time)
-        if 1 == print_cscope_and_ctags_info:
+        use_time_str = "cscope Use time = %s s" % (normal_end_time - s_time)
+        if 1 == show_message_enable:
             Warnin_print(use_time_str)
         else:
             debug_backrun_python_print(use_time_str)
         debug_backrun_python_print("end for cscope")
+
+def ctags_task_func(show_message_enable, s_time):
+
+    #wait cscope_task_func touch cscope.files
+    i = 0
+    while ( (not os.path.exists('cscope.files')) or s_time > os.stat('cscope.files').st_mtime):
+        time.sleep(0.2)
+        i = i + 1
+        debug_backrun_python_print("wait cscope_task_func touch cscope.files")
+        if i > 200:
+            Warnin_print("we think issue may happened caused by cscope.files touched err after 40s")
+            clear_lock_i()
+            return -1
+
+    #wait cscope.files update end
+    i = 0
+    while True:
+        i = i + 1
+        debug_backrun_python_print("cscope.files update...")
+        os.system("sync")
+        before_time = os.stat('cscope.files').st_mtime
+        time.sleep(1)
+        os.system("sync")
+        check_time = os.stat('cscope.files').st_mtime
+        if before_time == check_time:
+            debug_backrun_python_print("cscope.files already update finish")
+            break
+        if i > 100:
+            Warnin_print("we think issue may happened caused by cscope.files \
+                    update so long time err after 60s:for example try to create tag at \
+                    so big dir eg : Android src root dir, which we do not suggest")
+            clear_lock_i()
+            return -1
 
     if 'cscope_and_ctags' == sys.argv[2]:
         debug_backrun_python_print("now for ctag")
@@ -217,7 +278,7 @@ def gen_cscope_and_ctag_file():
         debug_backrun_python_print(handle_tags_files_cmd)
         os.system(handle_tags_files_cmd)
 
-        if 0 == print_cscope_and_ctags_info:
+        if 0 == show_message_enable:
             ctags_cmd = "ctags -R --fields=+lS -L tags.files -f .auto_cscope_ctags/tags; rm tags.files"
             ctags_cmd = ctags_cmd + " 1>/dev/null  2>&1"
             ctags_cmd = ctags_cmd + "; mv .auto_cscope_ctags/tags ./"
@@ -228,19 +289,12 @@ def gen_cscope_and_ctag_file():
         debug_backrun_python_print("show print_message :cmd %s" % ctags_cmd)
         os.system(ctags_cmd)
         ctags_end_time  = time.time()
-        ctags_use_time_str = "Ctags Use time = %s s" % (ctags_end_time - normal_end_time)
-        if 1 == print_cscope_and_ctags_info:
+        ctags_use_time_str = "Ctags Use time = %s s" % (ctags_end_time - s_time)
+        if 1 == show_message_enable:
             Warnin_print(ctags_use_time_str)
         else:
             debug_backrun_python_print(ctags_use_time_str)
         debug_backrun_python_print("end for ctag")
-
-    end_remove_lock_cmd = "rm .auto_cscope_ctags/lock 1>/dev/null  2>&1"
-    debug_backrun_python_print("now for remove_lock_cmd")
-    os.system(end_remove_lock_cmd)
-    debug_backrun_python_print("end for remove_lock_cmd")
-
-    gnome_osd_print('%s project update tags end' % sys.argv[1])
 
 #############################################start here
 check_args()
